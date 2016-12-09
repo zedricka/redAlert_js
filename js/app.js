e0fceef37b199c325aae7e52603fe701c29e7cea
zedricka <zedrick.ara@gmail.com>
2016-12-09 09:03:07 -1000

    wow

--- js/app.js
+++ js/app.js
@@ -3,5 +3,13 @@ console.log("Sanity check");
 function hello() {
   console.log("hello");
   var changeDiv = document.getElementById('divElement');
+  changeDiv.innerHTML = "HeheXD";
   console.log(changeDiv);
+}
+
+function soFast(){
+  console.log('Hello');
+  var classElements = document.getElementsByClassName('wow');
+  console.log(classElements);
+  classElements[0].innerHTML = "wat";
 }
